bc57a2879ffb1be17a9499b9c32b916bcaaf1c07
MiguelAngelLuque <miguel.luque.2014@upb.edu.co>
2018-06-13 17:53:29 -0500

    Initial commit

---
+++ src/chatcliente/HiloCliente.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatcliente;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ *
+ * @author admin
+ */
+public class HiloCliente implements Runnable{
+    Socket socketDatos;
+    HiloCliente(Socket socketConectado){
+    socketDatos = socketConectado;
+    }
+    
+    public void run(){
+        
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(socketDatos.getInputStream()));
+            while(true){
+                String r = br.readLine();
+                System.out.println("Respuesta: " + r);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        
+    
+    }
+    
+    
+}
